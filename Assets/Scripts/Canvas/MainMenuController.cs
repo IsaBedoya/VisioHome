38240926dee3a67c94388379aa42385266ef1efb
CodeInTheIzzyverse <dev.izzyverse@gmail.com>
2026-03-05 10:38:31 -0500

    Corregido y actualizado GUI

--- Assets/Scripts/Canvas/MainMenuController.cs
+++ Assets/Scripts/Canvas/MainMenuController.cs
@@ -1,21 +1,55 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using TMPro;
+using UnityEngine.UIElements;
 using System.Collections;
 
 public class MainMenuController : MonoBehaviour
 {
-    public TMP_InputField codeInput;
-    public UnityEngine.UI.Toggle furnishedToggle;
+    public UIDocument uiDocument;
+    private TextField codeInput;
+    private Toggle furnishedToggle;
+    private Toggle minimapToggle;
+
+    private Button enterBtn;
+    private Button exitBtn;
+    private Button helpBtn;
+
     public XRLoaderController xrLoader;
 
     public static bool isFurnished;
+    public static bool useMinimap;
     public static string apartmentCode;
 
-    public void OnEnterPressed()
+    void Start()
     {
-        apartmentCode = codeInput.text;
-        isFurnished = furnishedToggle.isOn;
+        var root = uiDocument.rootVisualElement;
+
+        if (xrLoader == null)
+        {
+            Debug.LogError("XRLoader no asignado");
+            return;
+        }
+
+        codeInput = root.Q<TextField>("CodeInput");
+
+        furnishedToggle = root.Q<Toggle>("ModeToggle");
+        furnishedToggle.value = true;
+        minimapToggle = root.Q<Toggle>("MinimapToggle");
+
+        enterBtn = root.Q<Button>("EnterBtn");
+        exitBtn = root.Q<Button>("ExitBtn");
+        helpBtn = root.Q<Button>("HelpBtn");
+
+        enterBtn.clicked += OnEnterPressed;
+        exitBtn.clicked += OnExitPressed;
+        helpBtn.clicked += OnHelpPressed;
+    }
+
+    void OnEnterPressed()
+    {
+        apartmentCode = codeInput.value;
+        isFurnished = furnishedToggle.value;
+        useMinimap = minimapToggle.value;
 
         StartCoroutine(StartVRAndLoad());
     }
@@ -29,8 +63,13 @@ public class MainMenuController : MonoBehaviour
         SceneManager.LoadScene("Apartment_A");
     }
 
-    public void OnExitPressed()
+    void OnExitPressed()
     {
         Application.Quit();
+    }
+
+    void OnHelpPressed()
+    {
+        Debug.Log("Mostrar ayuda (aún no implementado)");
     }
 }
